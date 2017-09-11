35050a5db115a6bc6a335d4c4e9bb20f811ff4c5
Josh Getter <getterj@mail.gvsu.edu>
2017-09-11 16:29:45 -0400

    Initial Commit

---
+++ Terminfo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Terminfo
+//
+//  Created by Josh Getter on 9/11/17.
+//  Copyright © 2017 Josh Getter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
