5d8e1cb390bddff2b45b3db5e388a624ce00675b
Josh Getter <getterj@mail.gvsu.edu>
2017-09-11 20:19:06 -0400

    Columns and rows working

--- Terminfo/main.c
+++ Terminfo/main.c
@@ -11,9 +11,11 @@
 #include <term.h>
 int main(int argc, const char * argv[]) {
     int err = 0;
+    //Setup the terminfo instance (null defaults to TERM environment variable).
     int retVal = setupterm(NULL, 1, &err);
-    printf("Output is : %d, error is: %d \n", retVal,err);
+    printf("Output is : %d, error is: %d \n", retVal,err); //DEBUGGING
     int numLines = tigetnum("lines");
-    printf("Number of lines is: %d \n",numLines);
+    int numCols = tigetnum("cols");
+    printf("There are: %d lines and %d columns\n",numLines,numCols);
     return 0;
 }
