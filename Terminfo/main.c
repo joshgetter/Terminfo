912235bb998ea6a0ecb365e40043d1017fa5b792
Josh Getter <getterj@mail.gvsu.edu>
2017-09-12 20:01:07 -0400

    Finished

--- Terminfo/main.c
+++ Terminfo/main.c
@@ -2,27 +2,29 @@
 //  main.c
 //  Terminfo
 //
-//  Created by Josh Getter on 9/11/17.
-//  Copyright © 2017 Josh Getter. All rights reserved.
+//  Josh Getter
+//  CIS 452 Terminfo
+//
 //
 
 #include <stdio.h>
 #include <curses.h>
 #include <term.h>
 #include <unistd.h>
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 int main(int argc, const char * argv[]) {
     int err = 0;
     //Setup the terminfo instance (null defaults to TERM environment variable).
     setupterm(NULL, 1, &err);
+    
     //1 Output term lines/cols
     int numLines = tigetnum("lines");
     int numCols = tigetnum("cols");
     printf("There are: %d lines and %d columns\n",numLines,numCols);
     
     //2 Sleep
-    sleep(2);
+    sleep(5);
     
     //init ncurses
     initscr();
@@ -30,25 +32,28 @@ int main(int argc, const char * argv[]) {
     //3 Clear screen/Home cursor
     clear();
     
-    //4 Prompt user
-    puts("Enter a coordinate (ex: \"x,y\"):\n");
-    refresh();
-    
-    //5 Get user input
-    char input[40];
-    fgets(input, 10, stdin);
-    char *xIn;
-    char *yIn;
-    xIn = strtok(input, ",");
-    //int x = atoi(xIn);
-    yIn = strtok(NULL, ",");
-    //int y = atoi(yIn);
-    
-    //6 Move cursor
-    move(y, x);
-    refresh();
-    
-    sleep(2);
+    bool running = true;
+    while(running){
+        //4 Prompt user
+        printw("Please enter a coordinate ex: \"x,y\" or \"q\" to quit. ");
+        
+        //5 Get user input
+        char input[40];
+        getstr(input);
+        running = strcmp(input, "q");
+        if(running){
+            char *xIn;
+            char *yIn;
+            xIn = strtok(input, ",");
+            yIn = strtok(NULL, ",");
+            int x = atoi(xIn);
+            int y = atoi(yIn);
+            
+            //6 Move cursor
+            move(y, x);
+            //refresh();
+        }
+    }
     endwin();
     return 0;
 }
